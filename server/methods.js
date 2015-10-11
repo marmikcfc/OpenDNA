29d35ce7fe359fc8882519e56ac975ef28922ac6
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2015-10-10 21:44:32 -0400

    fixed paren bug

--- server/methods.js
+++ server/methods.js
@@ -1,14 +1,11 @@
 Meteor.methods({
-  'txt2json': function () {
+	'txt2json': function () {
+		var dna = Meteor.npmRequire('dna2json');
+		var fs = Meteor.npmRequire('fs');
+		var path = Meteor.npmRequire('path');
 
-var dna = Meteor.npmRequire('dna2json');
-var fs = Meteor.npmRequire('fs');
-var path = Meteor.npmRequire('path');
-
-var JSONStream = Meteor.npmRequire('JSONStream');
-console.log(path.join(__meteor_bootstrap__.serverDir,'../web.browser/app')); 
-fs.createReadStream(path.join(__meteor_bootstrap__.serverDir, "../web.browser/app")+"/dna.txt").pipe(dna.createParser()).pipe(JSONStream.stringify()).pipe(fs.createWriteStream(path.join(__meteor_bootstrap__.serverDir,'dna1.json'));
-
-}
-
+		var JSONStream = Meteor.npmRequire('JSONStream');
+		console.log(path.join(__meteor_bootstrap__.serverDir,'../web.browser/app')); 
+		fs.createReadStream(path.join(__meteor_bootstrap__.serverDir, "../web.browser/app")+"/dna.txt").pipe(dna.createParser()).pipe(JSONStream.stringify()).pipe(fs.createWriteStream(path.join(__meteor_bootstrap__.serverDir,'dna1.json')));
+	}
 });
