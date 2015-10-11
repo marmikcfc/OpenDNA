182e6e34abb0b65bbca715bc47e3b3ae5ea53e27
marmik pandya <marmikpandya@gmail.com>
2015-10-11 01:38:33 -0400

    Matching logic started

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 var norovirus = Meteor.npmRequire('genoset-norovirus');
 
 var path = Meteor.npmRequire('path');
+var gql = Meteor.npmRequire('gql');
 
 
+var result= Risks.findOne({});
 
-var dna = Meteor.npmRequire(path.join(__meteor_bootstrap__.serverDir, "../web.browser/app")+"/dna.json");
+var dna = Meteor.npmRequire(path.join(__meteor_bootstrap__.serverDir,'../web.browser/app/dna1.json'));
+
+result['DNARiskValues']
+var query = gql.query();
+  	query.needs(0);
+  	query.and(query.exact('rs6025', 'A'), query.exact('rs1800595', 'C'));
+
 
 //var dna=HTTP.get(Meteor.absoluteUrl("./dna.json")).data;
 var data = norovirus(dna);
