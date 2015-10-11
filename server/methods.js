7e22a616e6a081e020ef5f598432d5ab62b901ae
marmik pandya <marmikpandya@gmail.com>
2015-10-10 22:20:09 -0400

    DNA2JSON works but app crashes

--- server/methods.js
+++ server/methods.js
@@ -1,11 +1,23 @@
 Meteor.methods({
-	'txt2json': function () {
-		var dna = Meteor.npmRequire('dna2json');
-		var fs = Meteor.npmRequire('fs');
-		var path = Meteor.npmRequire('path');
+  'txt2json': function () {
 
-		var JSONStream = Meteor.npmRequire('JSONStream');
-		console.log(path.join(__meteor_bootstrap__.serverDir,'../web.browser/app')); 
-		fs.createReadStream(path.join(__meteor_bootstrap__.serverDir, "../web.browser/app")+"/dna.txt").pipe(dna.createParser()).pipe(JSONStream.stringify()).pipe(fs.createWriteStream(path.join(__meteor_bootstrap__.serverDir,'dna1.json')));
-	}
+var dna = Meteor.npmRequire('dna2json');
+var fs = Meteor.npmRequire('fs');
+var path = Meteor.npmRequire('path');
+
+var JSONStream = Meteor.npmRequire('JSONStream');
+/*console.log(path.join(__meteor_bootstrap__.serverDir,'../web.browser/app')); 
+fs.createReadStream(path.join(__meteor_bootstrap__.serverDir, "../web.browser/app")+"/dna.txt").pipe(dna.createParser()).pipe(JSONStream.stringify()).pipe(fs.createWriteStream(path.join(__meteor_bootstrap__.serverDir,'dna1.json'));
+*/
+
+var txt = fs.readFileSync(path.join(__meteor_bootstrap__.serverDir, '../web.browser/app/dna1.txt'), 'utf8');
+console.log(typeof txt);
+
+dna.parse(txt, function(err, snps){
+  fs.writeFileSync(path.join(__meteor_bootstrap__.serverDir, '../web.browser/app/dna1.json'), JSON.stringify(snps));
+});
+
+
+}
+
 });
