91961f20bafa60340bb3ad0172f8fa413147ad02
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2015-10-11 15:57:42 -0400

    fixed user directory

--- client/uploadDNA/uploadDNA.js
+++ client/uploadDNA/uploadDNA.js
@@ -4,12 +4,12 @@ Meteor.startup(function() {
     console.log(typeof fileInfo.name);
     var filename= fileInfo.name.substring(0, fileInfo.name.length - 4);
     console.log(filename);
-    Meteor.call('txt2json',filename,function (err, response) {
-  console.log(response);
+    //Meteor.call('txt2json',filename,function (err, response) {  
+  //console.log(response);
 });
   //  Meteor.call('dnaAnalysis',filename);
-    console.log(fileInfo.name);
-    Meteor.call('txt2json',fileInfo.name);
+    //console.log(fileInfo.name);
+    //Meteor.call('txt2json',fileInfo.name);
   }
 })
 
