3f7972cd1043031dcb4b65356cc92e1f7281d392
marmik pandya <marmikpandya@gmail.com>
2015-10-11 22:23:17 -0400

    GQL working, Almost

--- server/methods.js
+++ server/methods.js
@@ -142,24 +142,31 @@ var cdPath = process.cwd() +'/uploads/' + Meteor.userId() + '/';
 var outPath=cdPath+filename+".json";
 var dna = Meteor.npmRequire(outPath);
 
-var result= function() {
-
-	return risks.findOne({});
-}
+var result= Risks.findOne({});
 
 console.log(result.DNARiskValues);
 
-var query = gql.or([
-  gql.exact('rs334', 'TT'),
-  gql.exact('i3003137', 'AA')
-]);
+
+
+for (var key in result.DNARiskValues) {
+  if (result.DNARiskValues.hasOwnProperty(key) && result.DNARiskValues[key]!="") {
+      //console.log("key   "+key+"value  "+result.DNARiskValues[key]);
+
+      console.log(gql.exact(key,result.DNARiskValues[key]));
+  }
+}
+
+//var abc=gql.exact('rs334', 'TT');
+//var pqr= gql.exact('i3003137', 'AA');
+
+var query = gql.or([abc,pqr]);
 
 var isMatch = query(dna);
 console.log(isMatch);
 
 var dna = Meteor.npmRequire(outPath);
 
-console.log("DNARiskValues     " + result['DNARiskValues']);
+console.log("DNARiskValues     " + result);
 
 
 
