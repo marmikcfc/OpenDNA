6f23578c1d6fc7f356d455dace6799989dc4cee0
marmik pandya <marmikpandya@gmail.com>
2015-10-12 01:22:21 -0400

    publications models created

--- server/methods.js
+++ server/methods.js
@@ -1,75 +1,51 @@
+
 Meteor.methods({
-  'txt2json': function (filename) {
-    var cdPath ='/uploads/' + Meteor.userId() + '/';
-    var inPath=cdPath+filename+".txt";
-    var outPath=cdPath+filename+".json" 
-    var outPath=cdPath+filename+".json"
+ 'txt2json': function (filename) {
+   var cdPath ='/uploads/' + Meteor.userId() + '/';
+   var inPath=cdPath+filename+".txt";
+   var outPath=cdPath+filename+".json" 
+   var outPath=cdPath+filename+".json"
 
-    shell.exec('dna2json '+inPath+" "+outPath, function(code, output) {
-  	 console.log(output);
-    });
-  },
-  'getUserFiles' : function() {
-    var cdPath = process.cwd() +  '/uploads/' + Meteor.userId() + '/';
-    var result = shell.exec('ls '+cdPath);
-    return result.output;
-  },
+   shell.exec('dna2json '+inPath+" "+outPath, function(code, output) {
+  console.log(output);
+   });
+ },
+ 'getUserFiles' : function() {
+   var cdPath = process.cwd() +  '/uploads/' + Meteor.userId() + '/';
+   var result = shell.exec('ls '+cdPath);
+   return result.output;
+ },
 
-  'dnaAnalysis': function (filename){
-    var path = Meteor.npmRequire('path');
-    var gql = Meteor.npmRequire('gql');
-    var cdPath = process.cwd() +'/uploads/' + Meteor.userId() + '/';
-    var outPath=cdPath+filename+".json";
-    var dna = Meteor.npmRequire(outPath);
+ 'dnaAnalysis': function (filename){
+   var path = Meteor.npmRequire('path');
+   var gql = Meteor.npmRequire('gql');
+   var cdPath = process.cwd() +'/uploads/' + Meteor.userId() + '/';
+   var outPath=cdPath+filename+".json";
+   var dna = Meteor.npmRequire(outPath);
 
-    var result= Risks.find().fetch();
+   var result= Risks.find().fetch();
 
-    var riskGroupAnalysis = new Object();
-    riskGroupAnalysis["userid"]=Meteor.userId();
+   var riskGroupAnalysis = new Object();
+   riskGroupAnalysis["userid"]=Meteor.userId();
 
-    var eachRiskGroupAnalysis = new Object();
-    _.each(result, function(res){
+   var eachRiskGroupAnalysis = new Object();
+   _.each(result, function(res){
 
-      var exactArray=new Array();
+     var exactArray=new Array();
 
-      for (var key in res.DNARiskValues) {
-        if (res.DNARiskValues.hasOwnProperty(key) && res.DNARiskValues[key]!="") {
-          exactArray.push(gql.exact(key,res.DNARiskValues[key]));
-        }
-      }
-      var query = gql.or(exactArray);
-      var isMatch = query(dna);
-      eachRiskGroupAnalysis[res.riskFactorName]=isMatch;
-    });
-    riskGroupAnalysis['eachRiskGroupAnalysis']= eachRiskGroupAnalysis;
-    Analysis.insert(riskGroupAnalysis);
-    console.log(Analysis.find().fetch());
-  }
-}
-
-//var abc=gql.exact('rs334', 'TT');
-//var pqr= gql.exact('i3003137', 'AA');
-
-var query = gql.or(exactArray);
-
-var isMatch = query(dna);
-//var matches=query.matches();
-var percentage=query.percentage();
-
-
-console.log(isMatch+" Percentages "+percentage);
-
-
-eachRiskGroupAnalysis[res.riskFactorName]=isMatch;
-
-
-
+     for (var key in res.DNARiskValues) {
+       if (res.DNARiskValues.hasOwnProperty(key) && res.DNARiskValues[key]!="") {
+         exactArray.push(gql.exact(key,res.DNARiskValues[key]));
+       }
+     }
+     var query = gql.or(exactArray);
+     var isMatch = query(dna);
+     var percentage=query.percentage();
+     console.log("percentage "+percentage);
+     eachRiskGroupAnalysis[res.riskFactorName]=isMatch;
+   });
+   riskGroupAnalysis['eachRiskGroupAnalysis']= eachRiskGroupAnalysis;
+   Analysis.insert(riskGroupAnalysis);
+   console.log(Analysis.find().fetch());
+ }
 });
-riskGroupAnalysis['eachRiskGroupAnalysis']= eachRiskGroupAnalysis;
-Analysis.insert(riskGroupAnalysis);
-console.log(Analysis.find().fetch());
-//console.log("DNARiskValues     " + result);
-
-
-
-}
